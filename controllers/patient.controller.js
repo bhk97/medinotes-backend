d8b1ac44964ee74985bf3ca6b84bf1ebf9042873
Bhagirath Kapdi <kapdibhagirath7@gmail.com>
2025-09-28 21:41:30 +0530

    login method changed

--- controllers/patient.controller.js
+++ controllers/patient.controller.js
@@ -67,7 +67,7 @@ exports.login = async(req,res,next)=>{
 		if(isMatch === false){
 			return res.json({status:false,result:"incorrect password"})
 		}
-		let tokenData = {userid:user.userid,email:user.email};
+		let tokenData = {userid:user.user_id,email:user.email};
 		const token = await patientService.generateToken(tokenData,process.env.SECRET_KEY,"1y");
 		res.status(200).json({status:true,token:token});
 	} catch(error){
